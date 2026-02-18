bb83e50e61cc1b8a33acbb7687b461d835880208
Yulia Karpynets <yukarpynets90@gmail.com>
2026-02-18 18:20:43 +0100

    HW4

---
+++ js/task-1.js
@@ -0,0 +1,26 @@
+function isEnoughCapacity(products, containerSize) {
+  let total = 0;
+
+  const quantities = Object.values(products);
+
+  for (const quantity of quantities) {
+    total += quantity;
+  }
+
+  return total <= containerSize;
+}
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
